924d9ed9a3fa0018d5a311aa346ca2d39ed67573
bill <b10902118@ntu.edu.tw>
2024-04-10 14:51:02 +0800

    functionize

--- main.cc
+++ main.cc
@@ -1,104 +1,92 @@
-#include<iostream>
-
+#include <iostream>
+#include <cstring>
 using namespace std;
 
-int main(){
-    int X,Y;
-    cin>>X>>Y;
+void printLine(int X, int loc = -1, char brick[] = (char *)"") {
+    if (brick) {
+        for (int i = 1; i < loc; ++i) cout << 0;
+        cout << brick;
+        for (int i = loc + strlen(brick); i <= X; ++i) cout << 0;
+    }
+    else {
+        for (int i = 1; i <= X; ++i) cout << 0;
+    }
+    cout << endl;
+}
+
+void blankLines(int n, int X) {
+    for (int i = 0; i < n; ++i) printLine(X);
+}
+
+bool rightClean(int X, int x, int d) { return (d != 1 && x < X - 1) || (d == 1 && x < X); }
+bool leftClean(int X, int x, int d) { return (d != 3 && x > 2) || (d == 3 && x > 1); }
+bool rotateClean(int X, int x, int y, int d) {
+    return !(d == 1 && x == X) && !(d == 3 && x == 1) && !(d == 2 && y == 1);
+}
+bool touchGround(int X, int x, int y, int d) { return (d != 2 && y == 2) || (d == 2 && y == 1); }
+int toGround(int d) { return d == 2 ? 1 : 2; }
+
+int main() {
+    int X, Y;
+    cin >> X >> Y;
     int n;
-    cin>>n;
+    cin >> n;
 
-    int x= X/2+X%2,y=Y,d=0;
+    int x = X / 2 + X % 2, y = Y, d = 0;
+    bool grounded = false;
 
-    for (int i=0;i<n;++i){
+    for (int i = 0; i < n && !grounded; ++i) {
         int op;
-        cin>>op;
+        cin >> op;
 
         --y;
 
-        switch (op){
+        switch (op) {
         case 1:
-            if (((d==0||d==2||d==3)&&(x<X-1))||(d==1&&x<X)) ++x;
+            if (rightClean(X, x, d)) ++x;
             break;
         case 2:
-            if (((d==0||d==2||d==1)&&(x>2))||(d==3&&x>1)) --x;
+            if (leftClean(X, x, d)) --x;
             break;
         case 3:
-            if (d==2) y=1;
-            else y=2;
-            goto RESULT;
+            y = toGround(d);
+            grounded = true;
         case 4:
-            if (!(d==1&&x==X)&&!(d==3&&x==1)&&!(d==2&&y==1)){
-                d=(d+1)%4;
+            if (rotateClean(X, x, y, d)) {
+                d = (d + 1) % 4;
             }
             break;
         }
-        if (((d==0||d==1||d==3)&&(y==2))||(d==2&&y==1)) break;
+        if (touchGround(X, x, y, d)) break;
     }
 
-RESULT:
-    if (d!=0){
-        for (int i=Y;i>y+1;--i){
-            for (int j=1;j<=X;++j) cout<<0;
-            cout<<endl;
-        }
+    if (d != 0) {
+        blankLines(Y - (y + 1), X);
 
-        for (int i=1;i<x;++i){
-            cout<<0;
-        }
-        cout<<1;
-        for (int i=x+1;i<=X;++i){
-            cout<<0;
-        }
-        cout<<endl;
+        printLine(X, x, (char *)"1");
     }
-    else{
-        for (int i=Y;i>y;--i){
-            for (int j=1;j<=X;++j) cout<<0;
-            cout<<endl;
+    else {
+        blankLines(Y - y, X);
+    }
+
+    if (d == 0 || d == 2) {
+        printLine(X, x - 1, (char *)"111");
+    }
+    else {
+        if (d == 1) {
+            printLine(X, x - 1, (char *)"11");
+        }
+        else {
+            printLine(X, x, (char *)"11");
         }
     }
 
-    if (d==0||d==2){
-        for (int i=1;i<x-1;++i) cout<<0;
-        cout<<"111";
-        for (int i=x+2;i<=X;++i) cout<<0;
-        cout<<endl;
+    if (d != 2) {
+        printLine(X, x, (char *)"1");
+        blankLines(y - 2, X);
     }
-    else{
-        if (d==1){
-            for (int i=1;i<x-1;++i) cout<<0;
-            cout<<"11";
-            for (int i=x+1;i<=X;++i) cout<<0;
-        }
-        else{
-            for (int i=1;i<x;++i) cout<<0;
-            cout<<"11";
-            for (int i=x+2;i<=X;++i) cout<<0;
-        }
-        cout<<endl;
-    }
-
-    if (d!=2){
-        for (int i=1;i<x;++i){
-            cout<<0;
-        }
-        cout<<1;
-        for (int i=x+1;i<=X;++i){
-            cout<<0;
-        }
-        cout<<endl;
-
-        for (int i=y-2;i>= 1;--i){
-            for (int j=1;j<=X;++j) cout<<0;
-            cout<<endl;
-        }
-    }
-    else{
-        for (int i=y-1;i>= 1;--i){
-            for (int j=1;j<=X;++j) cout<<0;
-            cout<<endl;
-        }
+    else {
+        blankLines(y - 1, X);
     }
 
     return 0;
